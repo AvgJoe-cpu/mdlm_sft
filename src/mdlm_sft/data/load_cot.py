4d7086a6715cac2f596df348d72a4875e4ac18f3
AvgJoe-cpu <129824187+AvgJoe-cpu@users.noreply.github.com>
2026-06-04 21:53:08 +0200

    .

--- src/mdlm_sft/data/load_cot.py
+++ src/mdlm_sft/data/load_cot.py
@@ -190,7 +190,7 @@ def split_dataset_by_ratios(dataset: Dataset, ratios: List[float]) -> DatasetDic
 try:
     # 1. Load + normalize
     raw_ds = load_dataset("avgJo3/Cot-collection-datasets-4.8.5", split="train")
-    raw_ds = raw_ds.select(range(1000000))  # Limit to 100k samples for memory management
+    raw_ds = raw_ds.select(range(2000000))  # Limit to 100k samples for memory management
     print(raw_ds[0])
 
     pilot_ds = (
